a11a95a201e1ceece67dc393e28561e60f1f28b5
manuelfouce <mfouce@gmail.com>
2021-09-15 15:33:18 +0200

    practice 1

---
+++ practice/plot2.R
@@ -0,0 +1,28 @@
+household_power_consumption <- read.table("household_power_consumption.txt", sep=";",head=TRUE)
+
+household_power_consumption$datetime <- as.POSIXct(as.character(paste(household_power_consumption$Date,household_power_consumption$Time)), 
+                                                   format="%d/%m/%Y %H:%M:%S")
+
+household_power_consumption$Date <- as.Date(household_power_consumption$Date, format= "%d/%m/%Y")
+
+household_power_consumption <- subset(household_power_consumption, 
+                                      Date == c("2007-02-01" , "2007-02-02"))
+
+
+
+with(household_power_consumption, 
+     plot(datetime,as.numeric(Global_active_power),
+          type = "l",
+          ylab= "Global Active Power (kilowatts)",
+          xlab= ""))
+
+
+dev.copy(png, file="plot2.png")
+
+dev.off()
+
+
+
+
+
+
---
+++ practice/plot3.R
@@ -0,0 +1,28 @@
+household_power_consumption <- read.table("household_power_consumption.txt", sep=";",head=TRUE)
+
+household_power_consumption$datetime <- as.POSIXct(as.character(paste(household_power_consumption$Date,household_power_consumption$Time)), 
+                                                   format="%d/%m/%Y %H:%M:%S")
+
+household_power_consumption$Date <- as.Date(household_power_consumption$Date, format= "%d/%m/%Y")
+
+household_power_consumption <- subset(household_power_consumption, 
+                                      Date == c("2007-02-01" , "2007-02-02"))
+
+
+
+plot(household_power_consumption$datetime,as.numeric(household_power_consumption$Sub_metering_1),
+     col="black", type = "l",xlab= "",ylab="Energy sub metering")
+lines(household_power_consumption$datetime,as.numeric(household_power_consumption$Sub_metering_2),col="Red", type = "l")                                        
+lines(household_power_consumption$datetime,as.numeric(household_power_consumption$Sub_metering_3),col="Blue", type = "l")  
+legend("topright",pch="-", col=c("black","blue","red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")) 
+
+
+dev.copy(png, file="plot3.png")
+
+dev.off()
+
+
+
+
+
+
